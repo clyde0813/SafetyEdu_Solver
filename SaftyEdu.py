341ffd2151b059fd7651786f8a0bb7d238deead0
unknown <drericcity@gmail.com>
2021-09-08 22:49:25 +0900

    2학기 버젼

--- SaftyEdu.py
+++ SaftyEdu.py
@@ -18,57 +18,24 @@ wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "chosen-single"))).click()
 wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="frmStudent"]/div[1]/div[1]/div/div/div/input'))).send_keys(
     "목포대학교")
 driver.find_element_by_xpath('//*[@id="frmStudent"]/div[1]/div[1]/div/div/div/input').send_keys(Keys.ENTER)
+wait2.until(EC.element_to_be_clickable((By.CLASS_NAME, "applicationBtn"))).click()
 
 main_page = driver.current_window_handle
-wait2.until(lambda driver: 2 == len(driver.window_handles))
-for i in driver.window_handles:
-    if i != main_page:
-        popup_page = i
-driver.switch_to.window(popup_page)
-driver.maximize_window()
+wait2.until(EC.element_to_be_clickable((By.CLASS_NAME, "courseBtn"))).click()
 print("팝업창 감지")
-wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ctrlBtn.volumeBtn.on"))).click()
-print("무음 ON")
-while True:
-    if len(driver.find_elements_by_class_name("textBubble.view")) > 0:
-        driver.find_element_by_class_name("textBubble.view").click()
-        print("자동 넘기기 완료")
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ctrlBtn.volumeBtn.on"))).click()
-    elif len(driver.find_elements_by_class_name("motion.quizStartBtn.sudden.view")) > 0:
-        driver.find_element_by_class_name("motion.quizStartBtn.sudden.view").click()
-        print("퀴즈 진입")
-        driver.find_element_by_class_name("option.option_1.option_o").click()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btns.correctBtn"))).click()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "textBubble.view"))).click()
-        if len(driver.find_elements_by_class_name("btns.correctBtn")) > 0:
-            driver.find_element_by_class_name("option.option_1.option_o").click()
-            wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btns.correctBtn"))).click()
-            wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "textBubble.view"))).click()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ctrlBtn.volumeBtn.on"))).click()
-    elif len(driver.find_elements_by_class_name("motion.quizStartBtn.view")) > 0:
-        print("마지막 퀴즈")
-        driver.find_element_by_class_name("motion.quizStartBtn.view").click()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "moveBtn.nextPageBtn"))).click()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ctrlBtn.volumeBtn.on"))).click()
 
-    elif len(driver.find_elements_by_class_name("textBubble.complete.view")):
-        if len(driver.window_handles) > 1:
-            print("학습 완료")
-            for i in driver.window_handles:
-                if i != main_page:
-                    driver.switch_to.window(i)
-                    driver.close()
-        driver.switch_to.window(main_page)
-        driver.refresh()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "courseBtn"))).click()
-        wait2.until(lambda driver: 2 == len(driver.window_handles))
-        for i in driver.window_handles:
-            if i != main_page:
-                popup_page = i
-        driver.switch_to.window(popup_page)
-        print("팝업창 감지")
-        driver.maximize_window()
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ctrlBtn.volumeBtn.on"))).click()
-        print("무음 ON")
-    else:
-        pass
+while True:
+    wait2.until(lambda driver: 2 == len(driver.window_handles))
+    for i in driver.window_handles:
+        if i != main_page:
+            popup_page = i
+    driver.switch_to.window(popup_page)
+    driver.maximize_window()
+    wait2.until(EC.element_to_be_clickable((By.CLASS_NAME, "textBubble.view")))
+    num = driver.find_element_by_class_name("pageNum.tPageNum").text
+    num = int(num) + 1
+    print(num)
+    driver.execute_script('opener.PageLast(' + str(num) + ');')
+    wait2.until(lambda driver: 1 == len(driver.window_handles))
+    driver.switch_to.window(main_page)
+
